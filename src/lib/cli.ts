137cbe0a27b360f8e82460884d1c09bab571720b
Edvinas <edvinasnazarovas@gmail.com>
2024-11-11 14:24:15 +0200

    replace cli and remove parser folder

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,24 +1,25 @@
-export class Parameter {
-    public flag: string;
+export class Flag {
+    public name: string;
     public description: string;
     public defaultValue: string | null;
 
-    constructor(flag: string, description: string, defaultValue: string | null = null) {
-        this.flag = flag;
+    constructor(name: string, description: string, defaultValue: string | null = null) {
+        this.name = name;
         this.description = description;
         this.defaultValue = defaultValue;
     }
 }
 
-type ArgMap = Record<string, string | null | undefined>;
+type ArgMap = Record<string, string | true | null | undefined>;
 
-type Action = (options: ArgMap) => void;
+type Action = (params: ArgMap) => void;
 
 export class Command {
     public name: string;
     public description: string;
     public action: Action;
-    public params: Parameter[] = [];
+    public flags: Flag[] = [];
+    public requiredArgs: Flag[] = [];
 
     constructor(name: string, description: string, action: Action) {
         this.name = name;
@@ -26,10 +27,15 @@ export class Command {
         this.action = action;
     }
 
-    public addParam(param: Parameter) {
-        this.params.push(param);
+    public addFlag(flag: Flag) {
+        this.flags.push(flag);
         return this;
     }
+
+    public addRequiredArg(arg: Flag) {
+        this.requiredArgs.push(arg);
+        return;
+    } 
 }
 
 export class CLI {
@@ -39,12 +45,12 @@ export class CLI {
         this.commands = [];
     }
 
-    addCommand(command: Command) {
+    public addCommand(command: Command) {
         this.commands.push(command);
         return this;
     }
 
-    parse(args: string[]) {
+    public parse(args: string[]) {
         const [commandName, ...restArgs] = args;
         const command = this.commands.find(cmd => cmd.name === commandName);
 
@@ -55,18 +61,38 @@ export class CLI {
 
         const argMap: ArgMap = {};
 
-        restArgs.forEach(arg => {
-            console.log("got this arg", arg);
+        let requiredArgIndex = 0;
+
+        for (const arg of restArgs) {
+            if (!arg.startsWith("--") && requiredArgIndex < command.requiredArgs.length) {
+                const argName = command.requiredArgs[requiredArgIndex].name;
+                argMap[argName] = arg;
+                requiredArgIndex++;
+            } else {
+                break;
+            }
+        }
+
+        for (let i = 0; i < restArgs.length; i++) { // Iterate through args and set arg value to the next arg, or to true if the next arg is not present or an option
+            const arg = restArgs[i];
             if (arg.startsWith("--")) {
-                let currentOption = command.params.find(param => param.flag === arg);
-                if (currentOption) {
-                    argMap[currentOption.flag] = arg;
+                const currentFlag = command.flags.find(flag => flag.name === arg);
+
+                if (currentFlag) {
+                    i++;
+                    const nextArg = restArgs[i];
+
+                    if (nextArg && !nextArg.startsWith("--")) {
+                        argMap[currentFlag.name] = nextArg;
+                    } else {
+                        argMap[currentFlag.name] = true;
+                    }
                 } else {
-                    console.error(`Incorrect option: ${arg}`); // TODO: Add custom logger here
+                    console.error(`Option not found: ${arg}`); // TODO: Add custom logger here
                     return;
                 }
             }
-        });
+        }
 
         command.action(argMap);
     }
