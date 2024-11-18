4b3b1a9cea39f462f98beffc06e174e7900a556f
Edvinas <edvinasnazarovas@gmail.com>
2024-11-18 12:48:02 +0200

    update readme

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const csvConverter_1 = require("./lib/csvConverter");
-const args = process.argv.slice(2);
-csvConverter_1.csvConverter.parse(args);
